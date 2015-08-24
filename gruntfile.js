caf98660881d2282be96eeb145d525df0584e1ea
iwatakeshi <iwatakeshi@gmail.com>
2015-08-23 20:03:07 -0600

    Update
    Updated dependencies.
    Removed redundant tests and kept the necessary existence test.
    Added jsconfig.json for VSCode.
    Added default gengo pack.

--- gruntfile.js
+++ gruntfile.js
@@ -1,28 +1,11 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
   grunt.initConfig({
-    stripJsonComments: {
-      dist: {
-        files: {
-          "tests/locales/routed/dest/en-us.json": "tests/locales/routed/src/en-us.json",
-          "tests/locales/routed/dest/ja.json": "tests/locales/routed/src/ja.json",
-          "tests/locales/unrouted/dest/en-us.json": "tests/locales/unrouted/src/en-us.json",
-          "tests/locales/unrouted/dest/en.json": "tests/locales/unrouted/src/en.json",
-          "tests/locales/unrouted/dest/ja.json": "tests/locales/unrouted/src/ja.json"
-        }
-      }
-    },
     jshint: {
       src: ['lib/**/*.js', 'parser/**/*.js', 'hapi/index.js', 'koa/index.js', 'modules/**/*.js', 'tests/**/*.js'],
       options: {
         jshintrc:'.jshintrc'
       }
-    },
-    'json-pretty': {
-      options: {
-        src: ['tests/locales/routed/dest/', 'tests/locales/unrouted/dest'],
-        indent: 2
-      },
     },
     "jsbeautifier": {
       "default": {
@@ -53,14 +36,12 @@ module.exports = function(grunt) {
     },
     jssemicoloned: {
 
-      files: ['lib/**/*.js', 'parser/**/*.js', 'hapi/index.js', 'koa/index.js', 'modules/**/*.js', 'tests/**/*.js']
+      files: ['lib/**/*.js', 'parser/**/*.js', 'hapi/index.js', 'koa/index.js', 'modules/**/*.js', 'test/**/*.js']
     }
   });
   grunt.registerTask('default', [
     'jsbeautifier',
-    'jshint',
-    'stripJsonComments',
-    'json-pretty'
+    'jshint'
   ]);
 
   grunt.registerTask('semicolon', ['jssemicoloned']);
--- test/index.js
+++ test/index.js
@@ -1,13 +1,33 @@
-var gengo = require('../express');
-var app = require('express')();
-var root = require('app-root-path');
 /*jshint strict:false*/
-app.use(gengo({
+/*global describe, it*/
+
+// Dependencies
+var assert = require('chai').assert;
+var pack = require('gengojs-default-pack');
+var request = require('supertest');
+var _ = require('lodash');
+var path = require('path');
+// Servers
+var koa = require('koa');
+var hapi = require('hapi');
+var express = require('express');
+var root = require('app-root-path');
+// App
+var app = {
+  koa: koa(),
+  hapi: new hapi.Server(),
+  express: express()
+};
+// Wrapper
+var gengo = {
+  express:require('../express'),
+  hapi: require('../hapi'),
+  koa: require('../koa')
+};
+// Options
+var options = {
   header: {
     supported: ['en-us', 'ja']
-  },
-  backend: {
-    directory: root + '/test/locales/'
   },
   parser: {
     type: '*'
@@ -15,26 +35,71 @@ app.use(gengo({
   router: {
     enabled: false
   }
-}));
+};
+// Koa router
+var router = require('koa-router')();
 
-app.use('/', function(req, res) {
-  /*res.send(req.__(
-    'You have {numPhotos, plural,' +
-    ' =0 {no photos.}=1 {one photo.}other {# photos.}}', {
-      numPhotos: 1000
-    }));*/
-  var __ = gengo.clone().__;
-  res.send({
-    '1': __('[greeting.informal.advanced].hey', 'hello',
-      'hello'),
-    '2': __('msgformat.photos', {
-      numPhotos: 1000
-    })
-  });
-  /*res.send();*/
-  /*res.send(req.__(
-    'Hello {{name}}', {
-      name: 'Takeshi'
-    }));*/
+describe('gengo', function() {
+  'use strict';
+  // API tests
+    describe('wrapper test', function() {
+      // Koa
+      describe('koa', function() {
+        app.koa.use(gengo.koa(options, pack()));
+        app.koa.use(function*(next) {
+          this.body = {
+            __: !_.isUndefined(this.__) &&
+              !_.isUndefined(this.request.__) &&
+              !_.isUndefined(this.req.__),
+            __l: !_.isUndefined(this.__l) &&
+              !_.isUndefined(this.request.__l) &&
+              !_.isUndefined(this.req.__l)
+          };
+          yield next;
+        });
+        describe('api', function() {
+          it('should exist', function(done) {
+            request(app.koa.listen()).get('/').expect({
+              __: true,
+              __l: true
+            }, done);
+          });
+        });
+      });
+      // Express
+      describe('express', function() {
+        app.express.use(gengo.express(options, pack()));
+        app.express.use(function(req, res) {
+          res.send({
+            __: !_.isUndefined(req.__) || false,
+            __l: !_.isUndefined(req.__l) || false
+          });
+        });
+
+        describe('api', function() {
+          it('should exist', function(done) {
+            request(app.express).get('/').expect({
+              __: true,
+              __l: true
+            }, done);
+          });
+        });
+      });
+      // Hapi
+      describe('hapi', function() {
+        app.hapi.connection({
+          port: 3000
+        });
+        app.hapi.register(gengo.hapi(options, pack()), function() {});
+        describe('api', function() {
+          it('should exist', function(done) {
+            app.hapi.inject('/', function(res) {
+              assert.isDefined(res.request.__);
+              assert.isDefined(res.request.__l);
+              done();
+            });
+          });
+        });
+      });
+    });
 });
-app.listen(3000);
